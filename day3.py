c910f565abccf5f1d330b0c3fe9967bae5e4493b
mike <mikebasdeo@gmail.com>
2020-04-02 19:29:37 -0400

    weird fix to get epochs to run.

--- day3.py
+++ day3.py
@@ -5,23 +5,24 @@
 
 # %%
 #  imports
-import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
 from keras.layers import Dense, Flatten
 from keras import Sequential
+from tensorflow import keras
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 print("tensorflow version = {}".format(tf.__version__))
 print("keras version={}".format(keras.__version__))
+np.set_printoptions(linewidth=5000)
 
 
 # %%
 # Load images
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-
-
-# %%
-np.set_printoptions(linewidth=5000)
+print(x_train.shape)
 
 
 # %%
@@ -30,7 +31,7 @@ print(x_train[0])
 
 
 # %%
-# matplotlib-prettyprint
+# matplotlib-prettyprint and corresponding label
 plt.imshow(x_train[50])
 print(y_train[50])
 
@@ -46,8 +47,24 @@ x_test = x_test/255.0
 model = Sequential()
 model.add(Flatten(input_shape=(28, 28)))
 model.add(Dense(units=256, activation='relu'))
+# output layer
+model.add(Dense(units=10, activation='softmax'))
 
 print(model.summary())
+
+
+# %%
+print(x_train.shape)
+
+
+# %%
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+# %%
+model.fit(x_train, y_train, epochs=20)
 
 
 # %%
